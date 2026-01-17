8f400f6fe9b5bd0455300fae222719362d1409d4
Karuna Dahal <karunadahal503@gmail.com>
2026-01-17 20:05:09 +0545

    chore: remove comments

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import path from 'path'
-
-// export default defineConfig({
-//   base: './',
-//   server: {
-//     port:3000
-//   },
-//   plugins: [react()],
-//   build: {
-//     outDir: 'dist', // ✅ Must match capacitor.config.js
-//   },
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//       '@components': path.resolve(__dirname, './src/components'),
-//       '@pages': path.resolve(__dirname, './src/pages'),
-//       '@assets': path.resolve(__dirname, './src/assets'),
-//       '@context': path.resolve(__dirname, './src/context'),
-//       '@utils': path.resolve(__dirname, './src/utils'),
-//       '@config': path.resolve(__dirname, './src/config'),
-//       '@services': path.resolve(__dirname, './src/services'),
-//     },
-//   },
-// })
-
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
